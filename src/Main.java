f8a8833c48ecd9af909051b98fe5e4e087c9955d
Ajay RajPut <ajayhbti11@gmail.com>
2024-08-14 19:12:25 +0530

    Date Time API

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Main {
@@ -23,6 +24,26 @@ public class Main {
         int nano=time.getNano();
 
         System.out.printf("%d:%d:%d:%d" ,hour,min,sec,nano);
+
+        LocalDateTime dt=LocalDateTime.now();
+        System.out.println(" ");
+        System.out.println("Date Time API ");
+
+        System.out.println(dt);
+
+        int dd=dt.getDayOfMonth();
+        int mm=dt.getMonthValue();
+        int yy=dt.getYear();
+        System.out.printf("Date : %d-%d-%d",dd,mm,yy);
+
+        System.out.println(" ");
+        int h=dt.getHour();
+        int m=dt.getMinute();
+        int s=dt.getSecond();
+        int n=dt.getNano();
+
+
+        System.out.printf("\n Time : %d:%d:%d:%d" ,h,m,s,n);
 
     }
 }
