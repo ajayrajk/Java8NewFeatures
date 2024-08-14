c7338ef93f257b01c9bd18a2e62fb89288912342
Ajay RajPut <ajayhbti11@gmail.com>
2024-08-14 18:25:41 +0530

    Date Time API

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        LocalDate date=LocalDate.now();
+        System.out.println("Current Date : " + date);
+
+        LocalTime time=LocalTime.now();
+        System.out.println("Current Time : " + time);
+
     }
 }
